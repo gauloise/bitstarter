617c4499f9d52bf687829e33a40d045f7c6ef630
John Smith <ubuntu@ip-172-31-37-141.us-west-2.compute.internal>
2013-07-13 09:55:16 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
-
-var express = require('express');
-
+var express = require('express')
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+app.get('/',function(request,response) {
+    var fs = require('fs');
+    var content = fs.readFileSync('index.html','utf-8');
+    response.send(content);
 });
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+ 
+var port = process.env.port || 5000;
+app.listen(port,function() {
+    console.log("Listening on " +port);
 });
