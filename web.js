768f3595248c8e108216353e9ce6c3fc8319cac9
gauloise <ggalatis11@gmail.com>
2013-07-13 21:46:53 +0300

    Update web.js

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = require('fs');
 
-var data = fs.readfilesync("index.html","utf-8");
+var data = fs.readFileSync("index.html", "utf-8");
 
 var app = express.createServer(express.logger());
-app.get('/',function(request,response) {
-    response.send(data);
+app.get('/', function(request, response) {
+  response.send(data);
 });
- 
-var port = process.env.port || 5000;
-app.listen(port,function() {
-    console.log("Listening on " +port);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
